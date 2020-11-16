364d6515c9c90a517c6e014d2c91ff5853c0c3aa
Kirill Krupina <kirill.krupina26@gmail.com>
2020-11-16 11:00:15 +0200

    Dispatch actions to the store

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import BookListItem from '../book-list-item';
+import {withBookstoreService} from '../hoc'
+
+import * as actions from '../../actions'
 
 import './book-list.css';
 
 class BookList extends Component {
+
+    componentDidMount() {
+        // 1. receive data
+        const {bookstoreService} = this.props;
+        const data = bookstoreService.getBooks();
+        console.log(data);
+
+        // 2. dispatch to store
+        this.props.booksLoaded(data);
+    }
+
     render() {
         const {books} = this.props;
         return (
@@ -28,4 +43,8 @@ const mapStateToProps = (state) => {
     }
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators(actions, dispatch);
+};
+
+export default withBookstoreService()(connect(mapStateToProps,mapDispatchToProps)(BookList));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,5 @@
 const initialState = {
-    books: [{
-        id: 1,
-        title: 'Harry Potter and the Philosophers Stone',
-        author: 'J.K. Rowling'
-    }, {
-        id: 2,
-        title: 'The Lord Of The Rings',
-        author: 'J.R.R Tolkien'
-    }]
+    books: []
 };
 
 const reducer = (state = initialState, action) => {
