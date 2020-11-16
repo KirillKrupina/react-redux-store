2139e4a506cc75223eda4b61355cab085e45295b
Kirill Krupina <kirill.krupina26@gmail.com>
2020-11-16 12:47:06 +0200

    Added new components and new styles

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -4,11 +4,15 @@ export default class BookstoreService {
         return [{
             id: 1,
             title: 'Harry Potter and the Philosophers Stone',
-            author: 'J.K. Rowling'
+            author: 'J.K. Rowling',
+            price: 7.99,
+            coverImage: 'https://images-na.ssl-images-amazon.com/images/I/81YOuOGFCJL.jpg'
         }, {
             id: 2,
             title: 'The Lord Of The Rings',
-            author: 'J.R.R Tolkien'
+            author: 'J.R.R Tolkien',
+            price: 9.99,
+            coverImage: 'https://images-na.ssl-images-amazon.com/images/I/8134AkhQJgL.jpg'
         }];
     }
 
